e7994b83d21a1d0ac147dbb9d4bdc1c006d5774a
nitish <nitish12@gmail.com>
2025-07-10 17:14:00 +0530

    Fix: use process.env.PORT for Render

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.use('/api/orders', orderRoutes);
 
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB Connected'))
